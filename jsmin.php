f89f32db50e943515c7361ebcfb6a2cacf7dc91a
Ryan Grove <ryan@wonko.com>
2009-10-13 02:54:53 +0000

    Prevent errors when mbstring.func_overload == 7 [patch contributed by Google Code user dhahler]

--- jsmin.php
+++ jsmin.php
@@ -138,7 +138,7 @@ class JSMin {
 
     if ($c === null) {
       if ($this->inputIndex < $this->inputLength) {
-        $c = $this->input[$this->inputIndex];
+        $c = substr($this->input, $this->inputIndex, 1);
         $this->inputIndex += 1;
       } else {
         $c = null;
