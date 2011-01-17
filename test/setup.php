56a15d3238b0c97e7602fdc1e6b4f865506b64c0
Ryan Grove <ryan@wonko.com>
2011-01-17 13:41:51 -0800

    Better equivalency test between jsmin.c and jsmin.php.

---
+++ test/setup.php
@@ -0,0 +1,30 @@
+#!/usr/bin/env php
+<?php
+$url_jsmin = 'https://github.com/douglascrockford/JSMin/raw/master/jsmin.c';
+
+$libs = array(
+  'dojo'     => 'https://ajax.googleapis.com/ajax/libs/dojo/1.5/dojo/dojo.xd.js.uncompressed.js',
+  'ext'      => 'https://ajax.googleapis.com/ajax/libs/ext-core/3.1.0/ext-core-debug.js',
+  'jquery'   => 'https://ajax.googleapis.com/ajax/libs/jquery/1.4.4/jquery.js',
+  'mootools' => 'https://ajax.googleapis.com/ajax/libs/mootools/1.3.0/mootools.js',
+  'yui'      => 'http://yui.yahooapis.com/3.3.0/build/yui/yui.js'
+);
+
+// Download latest JSMin and compile it.
+echo "Fetching $url_jsmin...\n";
+file_put_contents(__DIR__ . '/jsmin.c', file_get_contents($url_jsmin));
+
+echo "Compiling jsmin.c...\n";
+if (system('cc jsmin.c -o jsmin') === false) {
+  die();
+}
+
+// Download libs.
+@mkdir(__DIR__ . '/libs', 0755);
+
+foreach($libs as $name => $url) {
+  echo "Fetching $url...\n";
+  file_put_contents(__DIR__ . "/libs/$name.js", file_get_contents($url));
+}
+
+echo "Done\n";
--- test/test.php
+++ test/test.php
@@ -1,10 +1,28 @@
+#!/usr/bin/env php
 <?php
 error_reporting(E_STRICT);
-
-fwrite(STDERR, memory_get_peak_usage(true)."\n");
-
 require '../jsmin.php';
-echo JSMin::minify(file_get_contents('ext-all-debug.js'));
 
-fwrite(STDERR, memory_get_peak_usage(true)."\n");
-?>
+$libs = array(
+  'dojo',
+  'ext',
+  'jquery',
+  'mootools',
+  'yui'
+);
+
+foreach ($libs as $lib) {
+  echo "Testing $lib ";
+
+  $jsmin_c   = shell_exec(__DIR__ . "/jsmin < libs/$lib.js");
+  $jsmin_php = JSMin::minify(file_get_contents(__DIR__ . "/libs/$lib.js"));
+
+  if ($jsmin_c === $jsmin_php) {
+    echo "[PASS]\n";
+  } else {
+    echo "[FAIL]\n";
+    echo "==> Output differs between jsmin.c and jsmin.php.\n";
+  }
+}
+
+echo "Done.\n";
